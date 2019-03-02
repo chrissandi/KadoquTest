b115e236d31496ef02a95977281942a4181f4775
Radiyya Dwisaputra <radiyyasaputra@gmail.com>
2019-03-03 00:56:57 +0700

    Add navbar

---
+++ resources/views/inc/nav.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="/index">{{config('app.name')}}</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/items">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cart">Cart</a>
+                </li>
+            </ul>
+            
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">
+                        <img src="https://img.icons8.com/cotton/28/000000/gender-neutral-user.png">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/cart">Login</a>
+                </li>
+            </ul>
+
+        </div>
+</nav>
+{{-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script> --}}
